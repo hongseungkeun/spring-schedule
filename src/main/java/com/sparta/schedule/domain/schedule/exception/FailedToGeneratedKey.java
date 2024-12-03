0fc79baf7a6b94be4ebbdd516e78c3e16c46e17f
SeuneKeun <eiziong1@gmail.com>
2024-12-03 16:45:25 +0900

    style: 빈 줄 제거

--- src/main/java/com/sparta/schedule/domain/schedule/exception/FailedToGeneratedKey.java
+++ src/main/java/com/sparta/schedule/domain/schedule/exception/FailedToGeneratedKey.java
@@ -1,6 +1,6 @@
 package com.sparta.schedule.domain.schedule.exception;
 
-public class FailedToGeneratedKey extends RuntimeException{
+public class FailedToGeneratedKey extends RuntimeException {
     public FailedToGeneratedKey(String message) {
         super(message);
     }
--- src/schedule.sql
+++ src/schedule.sql
@@ -1,7 +1,6 @@
 create table schedule
 (
-    schedule_id bigint auto_increment
-        primary key,
+    schedule_id bigint auto_increment primary key,
     title       varchar(255)                       not null,
     todo        varchar(255)                       null,
     user_name   varchar(255)                       null,
