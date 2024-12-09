ff506e7b04d29eadff5ad1c5302fd65c2fa25499
SeuneKeun <eiziong1@gmail.com>
2024-12-09 11:13:05 +0900

    fix: 할일이 String 타입이기 때문에 @Max -> @Size 의 max 를 사용하도록 변경

--- src/main/java/com/sparta/schedule/domain/schedule/dto/request/ScheduleCreateReq.java
+++ src/main/java/com/sparta/schedule/domain/schedule/dto/request/ScheduleCreateReq.java
@@ -1,14 +1,14 @@
 package com.sparta.schedule.domain.schedule.dto.request;
 
 import com.sparta.schedule.domain.schedule.dto.ScheduleValidationMessages;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ScheduleCreateReq(
         @NotBlank(message = ScheduleValidationMessages.TITLE_BLANK_MESSAGE)
         String title,
 
-        @Max(value = ScheduleValidationMessages.TODO_MAX, message = ScheduleValidationMessages.TODO_MAX_MESSAGE)
+        @Size(max = ScheduleValidationMessages.TODO_MAX, message = ScheduleValidationMessages.TODO_MAX_MESSAGE)
         @NotBlank(message = ScheduleValidationMessages.TODO_BLANK_MESSAGE)
         String todo
 ) {
--- src/main/java/com/sparta/schedule/domain/schedule/dto/request/ScheduleUpdateReq.java
+++ src/main/java/com/sparta/schedule/domain/schedule/dto/request/ScheduleUpdateReq.java
@@ -1,11 +1,11 @@
 package com.sparta.schedule.domain.schedule.dto.request;
 
 import com.sparta.schedule.domain.schedule.dto.ScheduleValidationMessages;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ScheduleUpdateReq(
-        @Max(value = ScheduleValidationMessages.TODO_MAX, message = ScheduleValidationMessages.TODO_MAX_MESSAGE)
+        @Size(max = ScheduleValidationMessages.TODO_MAX, message = ScheduleValidationMessages.TODO_MAX_MESSAGE)
         @NotBlank(message = ScheduleValidationMessages.TODO_BLANK_MESSAGE)
         String todo
 ) {
